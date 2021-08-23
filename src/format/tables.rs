d21ac058dea9e5072d1beab420868c3eb11b5431
Ayose <ayosec@gmail.com>
2021-08-23 20:22:28 +0100

    Skip ANSI sequences when compute column widths
    This allows using CSI sequences to colorize columns.

--- src/format/tables.rs
+++ src/format/tables.rs
@@ -2,9 +2,10 @@
 //!
 //! Rows as separated by `\n`, and columns by `\t`.
 
+use crate::bytetables::ByteTable;
 use std::io::{self, Write};
 use std::{mem, str};
-use unicode_width::UnicodeWidthStr;
+use unicode_width::UnicodeWidthChar;
 
 /// Padding between columns.
 const PADDING: usize = 2;
@@ -42,7 +43,7 @@ impl<T: Write> Write for TableWriter<T> {
 
         for line in &lines {
             for (n, column) in line.split('\t').enumerate() {
-                let width = UnicodeWidthStr::width(column) + PADDING;
+                let width = display_width(column) + PADDING;
                 match widths.get_mut(n) {
                     Some(col) if *col < width => *col = width,
                     None => widths.push(width),
@@ -59,7 +60,13 @@ impl<T: Write> Write for TableWriter<T> {
         // Print table.
         for line in &lines {
             for (column, width) in line.split('\t').zip(&widths) {
-                write!(self.output, "{:1$}", column, width)?;
+                write!(self.output, "{}", column)?;
+
+                if let Some(width) = width.checked_sub(display_width(column)) {
+                    for _ in 0..width {
+                        self.output.write_all(b" ")?;
+                    }
+                }
             }
 
             self.output.write_all(&[b'\n'])?;
@@ -67,6 +74,28 @@ impl<T: Write> Write for TableWriter<T> {
 
         Ok(())
     }
+}
+
+/// Compute the width of a string, but skip ANSI sequences.
+fn display_width(s: &str) -> usize {
+    const ANSI_PARAMS: ByteTable = ByteTable::new(b"0-9:;[?!\"'#%()*+ ");
+
+    let mut width = 0;
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() {
+        if c == '\x1b' {
+            for c in &mut chars {
+                if !ANSI_PARAMS.contains(c as u8) {
+                    break;
+                }
+            }
+            continue;
+        }
+
+        width += UnicodeWidthChar::width(c).unwrap_or(0);
+    }
+
+    width
 }
 
 #[test]
@@ -84,4 +113,10 @@ fn render_test() {
          a    bbbb  \n\
          a    b     cccc\n"
     );
+}
+
+#[test]
+fn skip_ansi_for_width() {
+    assert_eq!(display_width("abc\x1b[md"), 4);
+    assert_eq!(display_width("0\x1b"), 1);
 }
